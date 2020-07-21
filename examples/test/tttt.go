310616b1c8cbb5733124feb7b23f2fc1a466cf76
marcetin <marcetin@gmail.com>
2020-07-22 01:05:18 +0200

    change

--- examples/test/tttt.go
+++ examples/test/tttt.go
@@ -5,6 +5,6 @@ import (
 )
 
 func main() {
-	d := db.DataBase("QmWC7wxt6z69ndm2JPYvkbp6xY6ZJ4ShaKfa98So82wzMy")
+	d := jdb.DataBase("QmWC7wxt6z69ndm2JPYvkbp6xY6ZJ4ShaKfa98So82wzMy")
 	d.Collection()
 }
