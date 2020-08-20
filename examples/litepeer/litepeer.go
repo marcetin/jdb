db9b55112d3ef1f9a3c2f0d16510221ccc09120e
marcetin <marcetin@gmail.com>
2020-08-21 01:07:33 +0200

    Change

--- examples/litepeer/litepeer.go
+++ examples/litepeer/litepeer.go
@@ -4,12 +4,20 @@ package main
 // hash from the IPFS network.
 
 import (
-	"gioui.org/app"
-	_ "gioui.org/app/permission/storage"
+	"context"
 	"gioui.org/font/gofont"
 	"gioui.org/op"
 	"gioui.org/unit"
 	"gioui.org/widget/material"
+	"io/ioutil"
+
+	ipfslite "github.com/hsanjuan/ipfs-lite"
+	"github.com/ipfs/go-cid"
+	crypto "github.com/libp2p/go-libp2p-core/crypto"
+	"github.com/multiformats/go-multiaddr"
+
+	"gioui.org/app"
+	_ "gioui.org/app/permission/storage"
 
 	"gioui.org/io/system"
 	"gioui.org/layout"
@@ -17,54 +25,54 @@ import (
 )
 
 func main() {
-	//ctx, cancel := context.WithCancel(context.Background())
-	//defer cancel()
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
 
 	// Bootstrappers are using 1024 keys. See:
 	// https://github.com/ipfs/infra/issues/378
-	//crypto.MinRsaKeyBits = 1024
+	crypto.MinRsaKeyBits = 1024
 
-	//ds, err := ipfslite.BadgerDatastore("./test")
-	//if err != nil {
-	//	panic(err)
-	//}
-	//priv, _, err := crypto.GenerateKeyPair(crypto.RSA, 2048)
-	//if err != nil {
-	//	panic(err)
-	//}
+	ds, err := ipfslite.BadgerDatastore("/tmp/stest")
+	if err != nil {
+		panic(err)
+	}
+	priv, _, err := crypto.GenerateKeyPair(crypto.RSA, 2048)
+	if err != nil {
+		panic(err)
+	}
 
-	//listen, _ := multiaddr.NewMultiaddr("/ip4/0.0.0.0/tcp/4005")
+	listen, _ := multiaddr.NewMultiaddr("/ip4/0.0.0.0/tcp/4005")
 
-	//h, dht, err := ipfslite.SetupLibp2p(
-	//	ctx,
-	//	priv,
-	//	nil,
-	//	[]multiaddr.Multiaddr{listen},
-	//	ds,
-	//	ipfslite.Libp2pOptionsExtra...,
-	//)
-	//
-	//if err != nil {
-	//	panic(err)
-	//}
-	//
-	//lite, err := ipfslite.New(ctx, ds, h, dht, nil)
-	//if err != nil {
-	//	panic(err)
-	//}
-	//
-	//lite.Bootstrap(ipfslite.DefaultBootstrapPeers())
+	h, dht, err := ipfslite.SetupLibp2p(
+		ctx,
+		priv,
+		nil,
+		[]multiaddr.Multiaddr{listen},
+		ds,
+		ipfslite.Libp2pOptionsExtra...,
+	)
 
-	//c, _ := cid.Decode("QmWATWQ7fVPP2EFGu71UkfnqhYXDYH566qy47CnJDgvs8u")
-	//rsc, err := lite.GetFile(ctx, c)
-	//if err != nil {
-	//	panic(err)
-	//}
-	//defer rsc.Close()
-	//content, err := ioutil.ReadAll(rsc)
-	//if err != nil {
-	//	panic(err)
-	//}
+	if err != nil {
+		panic(err)
+	}
+
+	lite, err := ipfslite.New(ctx, ds, h, dht, nil)
+	if err != nil {
+		panic(err)
+	}
+
+	lite.Bootstrap(ipfslite.DefaultBootstrapPeers())
+
+	c, _ := cid.Decode("QmWATWQ7fVPP2EFGu71UkfnqhYXDYH566qy47CnJDgvs8u")
+	rsc, err := lite.GetFile(ctx, c)
+	if err != nil {
+		panic(err)
+	}
+	defer rsc.Close()
+	content, err := ioutil.ReadAll(rsc)
+	if err != nil {
+		panic(err)
+	}
 
 	//fmt.Println(string(content))
 
@@ -85,7 +93,7 @@ func main() {
 				case system.FrameEvent:
 					gtx := layout.NewContext(&ops, e)
 
-					material.H3(th, string("mizengija")).Layout(gtx)
+					material.H3(th, string(content)).Layout(gtx)
 
 					e.Frame(gtx.Ops)
 				}
